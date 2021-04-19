2fc4cf3ffbb368170edb112e1966c47b1362ae08
yeksworld <yeksworld@gmail.com>
2021-04-19 16:52:09 +0300

    update0.2

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -10,7 +10,9 @@ def setup(browser):
     elif browser == 'firefox':
         driver = webdriver.Firefox(executable_path="C:/Users/yunus/AppData/Local/Drivers/geckodriver")
         print("Launching Firefox browser...........")
-
+    else:
+        driver = webdriver.Ie(executable_path="C:/Users/yunus/AppData/Local/Drivers/msedgedriver")
+        print("Launching Edge browser...........")
 
     return driver
 
@@ -22,3 +24,17 @@ def pytest_addoption(parser):  # This will get value from CLI /hooks
 @pytest.fixture()
 def browser(request):  # This will return the browser value to setup method
     return request.config.getoption("--browser")
+
+############# PyTest HTML Report ####################
+# It is hook for Adding Environment info to HTML Report
+
+def pytest_configure(config):
+    config._metadata['Project Name'] = 'nop Commerce'
+    config._metadata['Module Name'] = 'Customers'
+    config._metadata['Tester'] = 'Pavan'
+
+# It is hook for delete/Modify Environment info to HTML Report
+@pytest.mark.optionalhook
+def pytest_metadata(metadata):
+    metadata.pop("JAVA_HOME",None)
+    metadata.pop("Plugins",None)
